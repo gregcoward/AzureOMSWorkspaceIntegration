2ef04bf2f26f0cacaf8963e4a2d30668fd088619
gregcoward <g.coward@f5.com>
2018-12-06 01:17:21 -0800

    update

--- deploy_waf_logger.sh
+++ deploy_waf_logger.sh
@@ -14,6 +14,9 @@ done
 
 sleep 120
 
+echo $wspaceid
+echo $wspacekey
+
 # check for existence of device-group
 response_code=$(curl -sku $user:$passwd -w "%{http_code}" -X GET -H "Content-Type: application/json" https://localhost/mgmt/tm/cm/device-group/~Common~Sync  -o /dev/null)
 
@@ -30,7 +33,7 @@ fi
  response_code=$(curl -sk -u $user:$passwd -w "%{http_code}" -X POST -H "Content-Type: application/json" https://localhost/mgmt/tm/sys/application/service/ -d '{"name": "azurecloudlogger","partition": "Common","deviceGroup": "none","strictUpdates": "disabled","template": "/Common/template f5.cloud_logger.v1.0.0","trafficGroup": "none","lists": [{"name": "logging_config__ltm_req_log_options","value": "CLIENT_IP SERVER_IP  HTTP_METHOD HTTP_URI VIRTUAL_NAME"}],"variables": [{"Name":"analytics_config__analytics_solution","value": "azure_oms"},{"Name": "analytics_config__log_type","value": "F5CustomLog"},{"Name": "analytics_config__shared_key","encrypted": "yes","value": "'"$wspacekey"'"},{"Name": "analytics_config__workspace","value": "'"$wspaceid"'"},{"Name": "basic__advanced","value": "no"},{"Name": "basic__help","value": "hide"},{"Name": "internal_config__hostname","value": "yes"},{"Name": "internal_config__port","value": "yes"},{"Name": "logging_config__asm_log_choice","value": "yes"},{"Name": "logging_config__asm_log_level","value": "log_all"},{"Name": "logging_config__dos_logs","value": "yes"},{"Name": "logging_config__ltm_req_log_choice","value": "yes"}]}' -o /dev/null)
 
 if [[ $response_code != 200  ]]; then
-     echo "Failed to install LTM policy; exiting."
+     echo "Failed to deploy iapp; exiting."
      exit
 fi
 
