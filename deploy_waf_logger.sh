f5c82748cdafa1196838b1df73222c743b0a047d
gregcoward <g.coward@f5.com>
2018-12-06 14:06:59 -0800

    Update deploy_waf_logger.sh

--- deploy_waf_logger.sh
+++ deploy_waf_logger.sh
@@ -10,6 +10,9 @@ do	case "$option"  in
     esac
 done
 
+echo $wspaceid
+echo $wspacekey
+
 # deploy cloudlogger iApp
 tmsh create sys application service f5cloudlogger template f5.cloud_logger.v1.0.0 lists add { logging_config__ltm_req_log_options { value { CLIENT_IP SERVER_IP HTTP_METHOD HTTP_URI VIRTUAL_NAME }}} variables add { analytics_config__analytics_solution { value azure_oms } analytics_config__log_type { value F5CustomLog } analytics_config__shared_key { encrypted no value $wspacekey } analytics_config__workspace { value $wspaceid } basic__advanced { value no } basic__help { value hide } internal_config__hostname { value yes } internal_config__port { value yes } logging_config__asm_log_choice { value yes } logging_config__asm_log_level { value log_illegal } logging_config__dos_logs { value yes } logging_config__ltm_req_log_choice {  value yes }}
 
