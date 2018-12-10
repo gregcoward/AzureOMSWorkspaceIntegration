5d91d37835c9181bc2a57c1432e0da53e413af4a
gregcoward <g.coward@f5.com>
2018-12-10 15:07:17 -0800

    Update deploy_waf.sh

--- deploy_waf.sh
+++ deploy_waf.sh
@@ -9,12 +9,7 @@ do	case "$option"  in
     esac
 done
 
-response_code= tmsh create sys application service f5sampleapphttp template f5.http.v1.2.0 lists add { asm__security_logging { value { \"/Common/f5cloudlogger.app/f5cloudlogger_remote_logging\" }}} tables add { basic__snatpool_members { } net__snatpool_members { } optimizations__hosts { } pool__hosts { column-names { name } rows {{ row { www.sample.net }}}} pool__members { column-names { addr port connection_limit } rows {{ row { $webserverPrivateAddress 80 0 }}}} server_pools__servers { } } variables add { asm__asm_template { value POLICY_TEMPLATE_RAPID_DEPLOYMENT } asm__language { value utf-8 } asm__use_asm { value  "#create_new#" } client__http_compression { value "#create_new#" } net__client_mode { value wan } net__server_mode { value lan } net__v13_tcp { value yes } pool__addr { value 0.0.0.0/0 } pool__pool_to_use { value "#create_new#" } pool__port { value 80 }  ssl__mode { value no_ssl } ssl_encryption_questions__advanced { value no } ssl_encryption_questions__help { value hide }}
-
-if [[ $response_code != 200  ]]; then
-    echo "Failed to deploy application; exiting with response code '"$response_code"'"
-    exit
-fi
+tmsh create sys application service f5sampleapphttp template f5.http.v1.2.0 lists add { asm__security_logging { value { \"/Common/f5cloudlogger.app/f5cloudlogger_remote_logging\" }}} tables add { basic__snatpool_members { } net__snatpool_members { } optimizations__hosts { } pool__hosts { column-names { name } rows {{ row { www.sample.net }}}} pool__members { column-names { addr port connection_limit } rows {{ row { $webserverPrivateAddress 80 0 }}}}} variables add { asm__asm_template { value POLICY_TEMPLATE_RAPID_DEPLOYMENT } asm__language { value utf-8 } asm__use_asm { value  \"/#create_new#\" } client__http_compression { value \"/#create_new#\" } net__client_mode { value wan } net__server_mode { value lan } net__v13_tcp { value yes } pool__addr { value 0.0.0.0/0 } pool__pool_to_use { value \"/#create_new#\" } pool__port { value 80 }  ssl__mode { value no_ssl } ssl_encryption_questions__advanced { value no } ssl_encryption_questions__help { value hide }}
 
 echo "Deployment complete."
 exit
